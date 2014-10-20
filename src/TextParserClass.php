ce2d76661962b4c2fa4e31129ffd82b8a06878e7
Ayman Bedair <aymanrb@gmail.com>
2014-10-20 18:23:54 +0200

    Changed the class version

--- src/TextParserClass.php
+++ src/TextParserClass.php
@@ -6,7 +6,7 @@
  * text files based on a predefined template using PHP regular expressions.
  * 
  * @author     Ayman R. Bedair <http://www.AymanRB.com>
- * @version    0.1-beta
+ * @version    0.2-beta
  * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  */
  
