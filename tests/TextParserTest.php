8bba340d7a8164494f24e4f58a1b48078dacd602
Ayman Bedair <aymanrb@gmail.com>
2014-10-23 12:52:50 +0200

    Added a new test for the Loggin g File setter method

--- tests/TextParserTest.php
+++ tests/TextParserTest.php
@@ -3,8 +3,8 @@
  * Text Parser Class PHP Unit Test Cases
  *
  * A php unit test cases class to test against the main PHP Text Parser class
- * 
- * @author     Ayman R. Bedair <http://www.AymanRB.com>
+ *
+ * @author  Ayman R. Bedair <http://www.aymanrb.com>
  *
  */
 
@@ -13,16 +13,16 @@ require_once(__DIR__ . '/../src/TextParserClass.php');
 
 class TextParserTest extends PHPUnit_Framework_TestCase {
 
- 		/**
-     * @covers            TextParser::__construct
-     * @uses    					TextParser::setTemplatesDir
-     * @expectedException Exception
+    /**
+     * @covers              TextParser::__construct
+     * @uses                TextParser::setTemplatesDir
+     * @expectedException   Exception
      */
     public function testExceptionIsRaisedForInvalidConstructorArguments(){
     	new TextParser(__DIR__ . '/DirectoryThatNeverExists');
     }
-    
-    
+
+
     /**
      * @covers	TextParser::parseText
      * @uses    TextParser::__construct
@@ -37,7 +37,7 @@ class TextParserTest extends PHPUnit_Framework_TestCase {
   		$parser = new TextParser(__DIR__ . '/templates');
   		$this->assertFalse($parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/noTemplate.txt')));
   	}
-  	
+
   	/**
      * @covers	TextParser::parseText
      * @uses    TextParser::__construct
@@ -53,7 +53,7 @@ class TextParserTest extends PHPUnit_Framework_TestCase {
   		$parsedValues = $parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/success.txt'));
   		$this->assertEquals(13, count($parsedValues));
   	}
-  	
+
   	/**
      * @covers	TextParser::parseText
      * @uses    TextParser::__construct
@@ -67,8 +67,22 @@ class TextParserTest extends PHPUnit_Framework_TestCase {
   	public function testTextParsingReturns(){
   		$parser = new TextParser(__DIR__ . '/templates');
   		$parsedValues = $parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/success.txt'));
-  		
+
   		$this->assertEquals($parsedValues['country'], htmlspecialchars($parsedValues['country'])); //Make sure no html scripts are returned
   		$this->assertEquals('2', $parsedValues['children']); //Make sure data is trimed on return
   	}
+
+
+    /**
+     * @covers              TextParser::__construct
+     * @uses                TextParser::setTemplatesDir
+     * @uses                TextParser::setLogFile
+     * @expectedException   Exception
+     */
+    public function testLoggingError(){
+        $parser = new TextParser(__DIR__ . '/templates');
+        $parser->setLogFile('noFile.log');
+    }
+
+    //MORE TESTS ON LOG METHODS HERE
 }
