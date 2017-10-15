a5efe0066fb756c31822b1de2af52d91e4a3ff46
Ayman Bedair <aymanrb@gmail.com>
2017-10-15 12:47:30 +0200

    typo fix and and some code cleeanup

--- tests/TextParserTest.php
+++ tests/TextParserTest.php
@@ -13,7 +13,7 @@ class TextParserTest extends TestCase {
 
     /**
      * @covers              TextParser::__construct
-     * @uses                TextParser::createTemplatesDirInterator
+     * @uses                TextParser::createTemplatesDirIterator
      * @expectedException   Exception
      */
     public function testExceptionIsRaisedForInvalidConstructorArguments(){
@@ -23,28 +23,16 @@ class TextParserTest extends TestCase {
 
     /**
      * @covers	TextParser::parseText
-     * @uses    TextParser::__construct
-     * @uses    TextParser::createTemplatesDirInterator
-     * @uses    TextParser::findTemplate
-     * @uses    TextParser::prepareTemplate
-     * @uses    TextParser::prepareText
-     * @uses    TextParser::prepareText
-     * @uses    TextParser::extractData
      */
   	public function testTextParsingFailure(){
   		$parser = new TextParser(__DIR__ . '/templates');
-  		$this->assertFalse($parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/noTemplate.txt')));
+  		$this->assertEmpty(
+  		    $parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/noTemplate.txt'))
+        );
   	}
 
     /**
      * @covers	TextParser::parseText
-     * @uses    TextParser::__construct
-     * @uses    TextParser::createTemplatesDirInterator
-     * @uses    TextParser::findTemplate
-     * @uses    TextParser::prepareTemplate
-     * @uses    TextParser::prepareText
-     * @uses    TextParser::prepareText
-     * @uses    TextParser::extractData
      */
   	public function testTextParsingSuccess(){
   		$parser = new TextParser(__DIR__ . '/templates');
@@ -54,19 +42,25 @@ class TextParserTest extends TestCase {
 
     /**
      * @covers	TextParser::parseText
-     * @uses    TextParser::__construct
-     * @uses    TextParser::createTemplatesDirInterator
-     * @uses    TextParser::findTemplate
-     * @uses    TextParser::prepareTemplate
-     * @uses    TextParser::prepareText
-     * @uses    TextParser::prepareText
-     * @uses    TextParser::extractData
+     */
+    public function testTextParsingWithSimilarityCheckSuccess(){
+        $parser = new TextParser(__DIR__ . '/templates');
+        $parsedValues = $parser->parseText(
+            file_get_contents(__DIR__ . '/test_txt_files/success.txt'),
+            true
+        );
+        $this->assertEquals(13, count($parsedValues));
+    }
+
+    /**
+     * @covers	TextParser::parseText
      */
   	public function testTextParsingReturns(){
   		$parser = new TextParser(__DIR__ . '/templates');
   		$parsedValues = $parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/success.txt'));
 
   		$this->assertEquals($parsedValues['country'], htmlspecialchars($parsedValues['country'])); //Make sure no html scripts are returned
-  		$this->assertEquals('2', $parsedValues['children']); //Make sure data is trimed on return
+        $this->assertEquals('2', $parsedValues['children']); //Make sure data is trimmed on return
+        $this->assertEquals('11 - 10 - 2014', $parsedValues['arrival_date']); //Make sure data format and whitespaces are preserved
   	}
 }
