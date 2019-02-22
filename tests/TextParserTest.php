c954e0725c1802f545c24dd6b84c450c4b35a8cb
Ayman Bedair <ayman.bedair@contorion.de>
2019-02-22 10:13:10 +0100

    Code formatting and cosmetics

--- tests/TextParserTest.php
+++ tests/TextParserTest.php
@@ -5,45 +5,47 @@ namespace aymanrb\UnstructuredTextParser\Tests;
 include_once __DIR__ . '/../vendor/autoload.php';
 
 use aymanrb\UnstructuredTextParser\TextParser;
-use PHPUnit\Framework\TestCase;
 use Exception;
+use PHPUnit\Framework\TestCase;
 
-class TextParserTest extends TestCase {
-
-
+class TextParserTest extends TestCase
+{
     /**
      * @covers              TextParser::__construct
      * @uses                TextParser::createTemplatesDirIterator
      * @expectedException   Exception
      */
-    public function testExceptionIsRaisedForInvalidConstructorArguments(){
-    	new TextParser(__DIR__ . '/DirectoryThatNeverExists');
+    public function testExceptionIsRaisedForInvalidConstructorArguments()
+    {
+        new TextParser(__DIR__ . '/DirectoryThatNeverExists');
     }
 
-
     /**
-     * @covers	TextParser::parseText
+     * @covers    TextParser::parseText
      */
-  	public function testTextParsingFailure(){
-  		$parser = new TextParser(__DIR__ . '/templates');
-  		$this->assertEmpty(
-  		    $parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/noTemplate.txt'))
+    public function testTextParsingFailure()
+    {
+        $parser = new TextParser(__DIR__ . '/templates');
+        $this->assertEmpty(
+            $parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/noTemplate.txt'))
         );
-  	}
+    }
 
     /**
-     * @covers	TextParser::parseText
+     * @covers    TextParser::parseText
      */
-  	public function testTextParsingSuccess(){
-  		$parser = new TextParser(__DIR__ . '/templates');
-  		$parsedValues = $parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/success.txt'));
-  		$this->assertEquals(13, count($parsedValues));
-  	}
+    public function testTextParsingSuccess()
+    {
+        $parser = new TextParser(__DIR__ . '/templates');
+        $parsedValues = $parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/success.txt'));
+        $this->assertEquals(13, count($parsedValues));
+    }
 
     /**
-     * @covers	TextParser::parseText
+     * @covers    TextParser::parseText
      */
-    public function testTextParsingWithSimilarityCheckSuccess(){
+    public function testTextParsingWithSimilarityCheckSuccess()
+    {
         $parser = new TextParser(__DIR__ . '/templates');
         $parsedValues = $parser->parseText(
             file_get_contents(__DIR__ . '/test_txt_files/success.txt'),
@@ -53,14 +55,19 @@ class TextParserTest extends TestCase {
     }
 
     /**
-     * @covers	TextParser::parseText
+     * @covers    TextParser::parseText
      */
-  	public function testTextParsingReturns(){
-  		$parser = new TextParser(__DIR__ . '/templates');
-  		$parsedValues = $parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/success.txt'));
+    public function testTextParsingReturns()
+    {
+        $parser = new TextParser(__DIR__ . '/templates');
+        $parsedValues = $parser->parseText(file_get_contents(__DIR__ . '/test_txt_files/success.txt'));
 
-  		$this->assertEquals($parsedValues['country'], htmlspecialchars($parsedValues['country'])); //Make sure no html scripts are returned
+        $this->assertEquals($parsedValues['country'],
+            htmlspecialchars($parsedValues['country'])); //Make sure no html scripts are returned
         $this->assertEquals('2', $parsedValues['children']); //Make sure data is trimmed on return
-        $this->assertEquals('11 - 10 - 2014', $parsedValues['arrival_date']); //Make sure data format and whitespaces are preserved
-  	}
+        $this->assertEquals(
+            '11 - 10 - 2014',
+            $parsedValues['arrival_date']
+        ); //Make sure data format and whitespaces are preserved
+    }
 }
