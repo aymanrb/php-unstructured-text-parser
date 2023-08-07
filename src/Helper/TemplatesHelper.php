c3ed8ab7e4ee7bb15e79eb8527bc653ee202d4ed
Ayman Bedair <aymanrb@gmail.com>
2023-08-07 10:15:08 +0200

    fix blind removal of backslashes from prepared custom patterns

--- src/Helper/TemplatesHelper.php
+++ src/Helper/TemplatesHelper.php
@@ -8,6 +8,15 @@ use aymanrb\UnstructuredTextParser\Exception\InvalidTemplatesDirectoryException;
 
 class TemplatesHelper
 {
+    private const REGEX_GENERIC_VARIABLE = '/\\\{%(.*)%\\\}/U'; //{%Var%}
+    private const REGEX_VARIABLE_WITH_PATTERN = '/\\\{%([^%]+):(.*)%\\\}/U'; //{%Var:Pattern%}
+    private const REGEX_PREPARED_VARIABLE_WITH_PATTERN = '/(\(\?[^)]*)./'; //(?<Var\>Pattern)
+    private const REGEX_ORPHAN_BACKSLASH = '/(?<!\\\\)\\\\(?!\\\\)/';
+    private const STR_SEARCH_TRIPLE_BACKSLASHES = '\\\\\\';
+
+    private const REPLACE_GENERIC_VARIABLE = '(?<$1>.*)'; //(?<Var>.*)
+    private const REPLACE_VARIABLE_WITH_PATTERN = '(?<$1>$2)'; //(?<Var>Pattern)
+
     private \FilesystemIterator $directoryIterator;
 
     public function __construct(string $templatesDir)
@@ -76,19 +85,26 @@ class TemplatesHelper
     {
         $templateText = preg_quote($templateText, '/');
 
-        // replace all {%Var:Pattern%} in the template with (?<Var>Pattern) regex vars
-        $templateText =  preg_replace('/\\\{%([^%]+):(.*)%\\\}/U', '(?<$1>$2)', $templateText);
+        $templateText =  preg_replace(
+            self::REGEX_VARIABLE_WITH_PATTERN,
+            self::REPLACE_VARIABLE_WITH_PATTERN,
+            $templateText
+        );
 
-        // remove the regex escaped characters of the provided patterns
         $templateText = preg_replace_callback(
-            '/(\(\?[^)]*)./',
+            self::REGEX_PREPARED_VARIABLE_WITH_PATTERN,
             function ($matches) {
-                return str_replace('\\', '', $matches[0]);
+                $variableWithPattern = preg_replace(self::REGEX_ORPHAN_BACKSLASH, '', $matches[0]);
+
+                return str_replace(self::STR_SEARCH_TRIPLE_BACKSLASHES, '\\', $variableWithPattern);
             },
             $templateText
         );
 
-        // replace all {%Var%} in the template with (?<Var>.*) regex vars
-        return preg_replace('/\\\{%(.*)%\\\}/U', '(?<$1>.*)', $templateText);
+        return preg_replace(
+            self::REGEX_GENERIC_VARIABLE,
+            self::REPLACE_GENERIC_VARIABLE,
+            $templateText
+        );
     }
 }
